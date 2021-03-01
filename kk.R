22ac542447073984ef0591480e9305b263593b05
dagoetz <42743534+dagoetz@users.noreply.github.com>
2021-03-01 17:36:07 -0500

    Delete kk.R

--- kk.R
+++
@@ -1,9 +0,0 @@
-library(zipcode)
-library(tidyverse)
-library(maps)
-library(viridis)
-library(ggthemes)
-devtools::install_github("hrbrmstr/albersusa")
-library(albersusa)
-fm<-Export <- read_csv("311.csv")#the file we just downloaded
-data(zipcode)
